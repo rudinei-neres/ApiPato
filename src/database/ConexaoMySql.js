0d457fb92bf0eecbc21e94fc7e492f7b462d38bf
rudinei-neres <rudinei_neres@estudante.sesisenai.org.br>
2024-12-02 19:48:06 -0300

    env

--- src/database/ConexaoMySql.js
+++ src/database/ConexaoMySql.js
@@ -1,20 +1,34 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+// Carrega as variáveis do .env
+dotenv.config();
 
 const dbConfig = {
-  host: process.env.MYSQL_HOST || "localhost",
-  port: process.env.MYSQL_PORT || "3306",
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PWD || "root",
-  database: process.env.MYSQL_DB || "ApiPato",
- 
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PWD,
+  database: process.env.MYSQL_DB,
 };
 
 class ConexaoMySql {
-  async getConexao() {
-    if (!ConexaoMySql.conexao) {
-      ConexaoMySql.conexao = await mysql.createConnection(dbConfig);
+  constructor() {
+    if (!ConexaoMySql.pool) {
+      ConexaoMySql.pool = mysql.createPool(dbConfig); // Criação do pool de conexões
     }
-    return ConexaoMySql.conexao;
+  }
+
+  async getConexao() {
+    try {
+      const conexao = await ConexaoMySql.pool.getConnection(); // Obtém uma conexão do pool
+      console.log("Conexão com o banco estabelecida.");
+      return conexao;
+    } catch (error) {
+      console.error("Erro ao obter conexão com o banco:", error);
+      throw error;
+    }
   }
 }
+
 export default ConexaoMySql;
