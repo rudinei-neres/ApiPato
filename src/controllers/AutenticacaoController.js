b02ff78dbd53be5f2841adb57de38815b2422246
rudinei-neres <rudinei_neres@estudante.sesisenai.org.br>
2024-12-02 21:53:00 -0300

    6

--- src/controllers/AutenticacaoController.js
+++ src/controllers/AutenticacaoController.js
@@ -7,46 +7,50 @@ class AutenticacaoController {
   async login(req, resp) {
     try {
       const { email, senha } = req.body;
-
+  
       if (!email || !senha) {
         resp.status(400).send("Email e Senha são obrigatórios.");
         return;
       }
-
+  
       const conexao = await new ConexaoMySql().getConexao();
       const comandoSql = "SELECT * FROM usuarios WHERE email = ?";
       const [resultado] = await conexao.execute(comandoSql, [email]);
-
+  
       const usuarioEncontrado = resultado[0];
-
+  
       if (!usuarioEncontrado) {
         resp.status(401).send("Email ou Senha incorreta.");
         return;
       }
-
-      // Verifica a senha usando bcrypt
+  
+      console.log("Senha fornecida:", senha);
+      console.log("Hash armazenado no banco:", usuarioEncontrado.senha);
+  
+      // Verifica a senha usando bcryptjs
       const senhaValida = await bcryptjs.compare(senha, usuarioEncontrado.senha);
       if (!senhaValida) {
         resp.status(401).send("Email ou Senha incorreta.");
         return;
       }
-
+  
       // Remove a senha do objeto antes de retornar
       delete usuarioEncontrado.senha;
-
+  
       // Gera um token JWT
       const token = jwt.sign(
         { id_usuario: usuarioEncontrado.id_usuario, email: usuarioEncontrado.email, role: usuarioEncontrado.role },
         process.env.JWT_SECRET, // Chave secreta do JWT
         { expiresIn: "8h" } // Tempo de expiração do token
       );
-
+  
       resp.send({ usuario: usuarioEncontrado, token });
     } catch (error) {
       console.error("Erro ao realizar login:", error);
       resp.status(500).send("Erro interno do servidor.");
     }
   }
+   
 
   // Verifica o usuário autenticado com base no token JWT
   async verificarUsuario(req, resp) {
