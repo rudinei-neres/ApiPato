890d55786a66501a27704f5d317a8d1918e752d9
rudinei-neres <rudinei_neres@estudante.sesisenai.org.br>
2024-12-04 16:14:42 -0300

    kw

--- utils/bancoDeDados.js
+++ utils/bancoDeDados.js
@@ -4,12 +4,12 @@ import mysql from 'mysql2/promise'; // Importando mysql2 com a interface de prom
 dotenv.config(); // Carrega variáveis de ambiente do arquivo .env
 
 // Criação da conexão com o pool de banco de dados
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'junction.proxy.rlwy.net', // Host do banco de dados
-  port: process.env.MYSQL_PORT || 48875, // Porta do banco de dados
-  user: process.env.MYSQL_USER || 'root', // Usuário do banco de dados
-  password: process.env.MYSQL_PWD || 'rUDFGzcCTByGhzUMTpfHZhFLPCXKcuD', // Senha do banco de dados
-  database: process.env.MYSQL_DB || 'railway', // Nome do banco de dados
+const db = mysql.createPool({
+  host: process.env.MYSQL_HOST , // Host do banco de dados
+  port: process.env.MYSQL_PORT , // Porta do banco de dados
+  user: process.env.MYSQL_USER , // Usuário do banco de dados
+  password: process.env.MYSQL_PWD , // Senha do banco de dados
+  database: process.env.MYSQL_DB , // Nome do banco de dados
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
