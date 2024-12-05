298dd1a2e5d7eccfe1f4256b6ea50c0920cbe20d
rudinei-neres <rudinei_neres@estudante.sesisenai.org.br>
2024-12-05 02:57:50 -0300

    casa

--- servicos/usuarioServico.js
+++ servicos/usuarioServico.js
@@ -1,46 +1,50 @@
-import Usuario from '../modelos/usuarioModelo.js'; // Usando import para importar o modelo de usuário
+
 import ConexaoMySql from '../utils/bancoDeDados.js'; // Certifique-se de que o caminho e o nome estão corretos
 
-
 const UsuarioServico = {
-
+  // Obtém um usuário pelo email
   async obterUsuario(email) {
-    return await Usuario.buscarPorEmail(email);
-  },
-  async obterUsuario(email) {
-    const connection = await new ConexaoMySql().getConexao();
-    const [usuario] = await connection.execute(
+    const [usuario] = await ConexaoMySql.execute(
       "SELECT * FROM usuarios WHERE email = ?",
       [email]
     );
     return usuario[0] || null;
   },
 
+  // Obtém um usuário pelo ID
   async obterUsuarioPorId(id) {
-    const connection = await new ConexaoMySql().getConexao();
-    const [usuario] = await connection.execute(
+    const [usuario] = await ConexaoMySql.execute(
       "SELECT * FROM usuarios WHERE id_usuario = ?",
       [id]
     );
     return usuario[0] || null;
   },
-  
-  
-  
+
+  // Atualiza o saldo do usuário
   async atualizarSaldo(email, novoSaldo) {
-    return await Usuario.atualizarSaldo(email, novoSaldo);
+    await ConexaoMySql.execute(
+      "UPDATE usuarios SET saldo = ? WHERE email = ?",
+      [novoSaldo, email]
+    );
+    return { mensagem: 'Saldo atualizado com sucesso.' };
   },
+
+  // Deleta um usuário pelo ID
   async deletarUsuario(id) {
-    return await Usuario.deletarUsuario(id);
+    await ConexaoMySql.execute(
+      "DELETE FROM usuarios WHERE id_usuario = ?",
+      [id]
+    );
+    return { mensagem: 'Usuário deletado com sucesso.' };
   },
+
+  // Cria um novo usuário
   async criarUsuario({ nome, email, telefone, senha }) {
     console.log('Criando usuário no banco:', { nome, email, telefone });
     const query = 'INSERT INTO usuarios (nome, email, telefone, senha) VALUES (?, ?, ?, ?)';
-    await ConexaoMySql.query(query, [nome, email, telefone, senha]);
-  }
-  
-
-
+    await ConexaoMySql.execute(query, [nome, email, telefone, senha]);
+    return { mensagem: 'Usuário criado com sucesso.' };
+  },
 };
 
 export default UsuarioServico; // Usando export default para exportar o serviço
